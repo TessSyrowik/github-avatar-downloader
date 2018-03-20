3facb35f8c8a66777712ea1a54d117a38e2056bb
vagrant <your@email.com>
2018-03-20 01:39:20 +0000

    starting the framework

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,16 @@
 var request = require("request");
 
 console.log("Welcome to the GitHub Avatar Downloader!");
+
+function getRepoContributors(repoOwner, repoName, callback) {
+  // ...
+}
+// changed cb to callback
+// this will handle asynch results returned from getRepoContributors
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
+
+getRepoContributors();
