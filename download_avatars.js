51459254cf8c69aef35a0482d836cd3edae4e0c6
vagrant <your@email.com>
2018-03-20 02:12:39 +0000

    step 4 update

--- download_avatars.js
+++ download_avatars.js
@@ -2,15 +2,23 @@ var request = require("request");
 
 console.log("Welcome to the GitHub Avatar Downloader!");
 
-function getRepoContributors(repoOwner, repoName, callback) {
-  // ...
+function getRepoContributors(repoOwner, repoName, cb) {
+   var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
+ request(url, function(err, res, body) {
+    cb(err, body);
+  });
+
+  // var options = {
+  //   url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+  //   headers: {
+  //     'User-Agent': 'request'
+  //   }
+
+  // }
+
 }
-// changed cb to callback
-// this will handle asynch results returned from getRepoContributors
 
-getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
-});
-
-getRepoContributors();
+ getRepoContributors("jquery", "jquery", function(err, result) {
+    console.log("Errors:", err);
+    console.log("Result:", result);
+    });
