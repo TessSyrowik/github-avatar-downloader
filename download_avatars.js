a01ed56c342770dd789aa9579f2fc5db704f69a4
vagrant <your@email.com>
2018-03-20 02:37:57 +0000

    step 6 complete

--- download_avatars.js
+++ download_avatars.js
@@ -1,20 +1,25 @@
 var request = require("request");
+var secrets = require("secrets")
 
 console.log("Welcome to the GitHub Avatar Downloader!");
 
 function getRepoContributors(repoOwner, repoName, cb) {
-   var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
- request(url, function(err, res, body) {
+
+  var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
+    request(url, function(err, res, body) {
+    cb(err, body);
+    });
+
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request'
+    }
+  };
+
+  request(options, function(err, res, body) {
     cb(err, body);
   });
-
-  // var options = {
-  //   url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
-  //   headers: {
-  //     'User-Agent': 'request'
-  //   }
-
-  // }
 
 }
 
@@ -22,3 +27,14 @@ function getRepoContributors(repoOwner, repoName, cb) {
     console.log("Errors:", err);
     console.log("Result:", result);
     });
+
+
+
+
+
+
+
+
+
+
+
