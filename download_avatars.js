1cd986d92ff20270eb445e5307ecba60a19e7de7
vagrant <your@email.com>
2018-03-20 05:13:35 +0000

    finally there smiley

--- download_avatars.js
+++ download_avatars.js
@@ -1,32 +1,31 @@
 var request = require("request");
-var secrets = require("secrets")
+var secrets = require("./secrets");
 
 console.log("Welcome to the GitHub Avatar Downloader!");
 
 function getRepoContributors(repoOwner, repoName, cb) {
-
   var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
-    request(url, function(err, res, body) {
-    cb(err, body);
-    });
 
   var options = {
-    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    url: url,
     headers: {
       'User-Agent': 'request'
     }
   };
 
   request(options, function(err, res, body) {
-    cb(err, body);
+    var jsonObj = JSON.parse(body);
+    cb(err, jsonObj);
   });
-
 }
 
- getRepoContributors("jquery", "jquery", function(err, result) {
-    console.log("Errors:", err);
-    console.log("Result:", result);
-    });
+// Anonymous function                      ⬇
+getRepoContributors("jquery", "jquery", function (err, result) {
+  for (var i = 0; i < result.length; i++) {
+    console.log(result[i].avatar_url);
+  }
+});
+
 
 
 
